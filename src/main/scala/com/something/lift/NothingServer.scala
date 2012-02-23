0fe438c3aabc3d3300939c0e3ded6401ae629edf
David Pollak <feeder.of.the.bears@gmail.com>
2012-02-23 14:36:41 -0800

    We need a singleton rather than a new instance per request

--- src/main/scala/com/something/lift/NothingServer.scala
+++ src/main/scala/com/something/lift/NothingServer.scala
@@ -7,6 +7,9 @@ import java.util.concurrent.Executors
 import org.jboss.netty.bootstrap.ServerBootstrap
 import java.net.InetSocketAddress
 import org.jboss.netty.channel.{Channels, ChannelPipeline, ChannelPipelineFactory, ChannelFactory}
+import net.liftweb.util.Helpers
+import net.liftweb.http.{LiftRules, LiftServlet}
+import net.liftweb.common.{Empty, Box}
 
 /**
  * Created by IntelliJ IDEA.
@@ -14,6 +17,8 @@ import org.jboss.netty.channel.{Channels, ChannelPipeline, ChannelPipelineFactor
  * Date: 2/22/12
  * Time: 10:04 PM
  */
+
+import MyTypes._
 
 object NothingServer extends App {
 
@@ -24,13 +29,36 @@ object NothingServer extends App {
     Executors.newCachedThreadPool()
   )
 
+      private def findObject(cls: String): Box[AnyRef] =
+    Helpers.tryo[Class[_]](Nil)(Class.forName(cls + "$")).flatMap {
+      c =>
+        Helpers.tryo {
+          val field = c.getField("MODULE$")
+          field.get(null)
+        }
+    }
+
+    val transientVarProvider: VarProvider =
+    findObject("net.liftweb.http.TransientRequestVarHandler").open_!.asInstanceOf[VarProvider]
+  val reqVarProvider: VarProvider =
+    findObject("net.liftweb.http.RequestVarHandler").open_!.asInstanceOf[VarProvider]
+
+  val nettyContext = new NettyContext
+
+
+  val liftLand = new LiftServlet(nettyContext)
+
+  LiftRules.setContext(nettyContext)
+
   private[this] val bootstrap = new ServerBootstrap(factory)
+
+  val handler = new NothingServerHandler(nettyContext, transientVarProvider, reqVarProvider, liftLand)
 
   bootstrap.setPipelineFactory(new ChannelPipelineFactory {
     def getPipeline: ChannelPipeline = Channels.pipeline(
       new HttpRequestDecoder,
       new HttpResponseEncoder,
-      new NothingServerHandler
+      handler
     )
   })
 
